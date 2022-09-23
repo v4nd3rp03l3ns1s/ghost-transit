3e7f3c35a1604d8309d6ba13e5a417db7c076677
M <v4nd3rp03l3ns1s@gmail.com>
2022-09-23 14:05:07 -0500

    feat: implemented bus route controllers

--- server/controllers/bus/busRoute.js
+++ server/controllers/bus/busRoute.js
@@ -1,2 +1,37 @@
+'use strict';
+
+const config = require('../config');
+const db = require('../../models/db');
 
 //get a list of all bus routes
+
+const busRouteController = {
+  getAllBusRoutes: async function (ctx) {
+    try {
+      const busRouteArray = await db.BusRoute.findAll({
+        attributes: ['routeID', 'routeName', 'routeColor']
+      });
+      ctx.body = busRouteArray;
+      ctx.status = 200;
+    } catch (err) {
+      ctx.body = err;
+      ctx.status = 500;
+    }
+  },
+  getBusRoute: async function (ctx) {
+    try {
+      console.log(ctx.query);
+      const busRouteResult = await db.BusRoute.findAll({
+        attributes: ['routeID', 'routeName', 'routeColor'],
+        where: {routeID: ctx.query.rt}
+      });
+      ctx.body = busRouteResult
+      ctx.status = 200;
+    } catch (err) {
+      ctx.body = err;
+      ctx.status = 500;
+    }
+  }
+}
+
+module.exports = busRouteController;
--- server/routers/index.js
+++ server/routers/index.js
@@ -4,17 +4,25 @@ const Router = require('koa-router');
 
 //import controllers
 const devController = require('../controllers/devUtility/devController');
+const busRouteController = require('../controllers/bus/busRoute');
 
 //initialize all routers
 const router = new Router();
 
-//dev routes
-router.get('/dev/populateTrainStations', devController.populateTrainStations);
-router.get('/dev/populateTrainStops', devController.populateTrainStops);
-router.get('/dev/populateBusRoutes', devController.populateBusRoutes);
-router.get('/dev/populateBusDirections', devController.populateBusDirections);
-router.get('/dev/populateBusStops', devController.populateBusStops);
+//user routes
 
-// rootRouter.use('/', devRouter.routes());
+//bus routes
+router.get('/bus/getAllBusRoutes', busRouteController.getAllBusRoutes);
+router.get('/bus/getBusRoute', busRouteController.getBusRoute);
+
+//train routes
+
+//dev routes used to populate db
+// router.get('/dev/populateTrainStations', devController.populateTrainStations);
+// router.get('/dev/populateTrainStops', devController.populateTrainStops);
+// router.get('/dev/populateBusRoutes', devController.populateBusRoutes);
+// router.get('/dev/populateBusDirections', devController.populateBusDirections);
+// router.get('/dev/populateBusStops', devController.populateBusStops);
+
 
 module.exports = { router };
