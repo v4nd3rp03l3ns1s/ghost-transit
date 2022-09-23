97ad128b967c7dcbac9041cc79f6785e817c6d89
M <v4nd3rp03l3ns1s@gmail.com>
2022-09-23 18:26:58 -0500

    feat: implementing create account function

--- server/controllers/user/user.js
+++ server/controllers/user/user.js
@@ -1,7 +1,24 @@
+'use strict';
 
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const User = require('../../models/user');
+const { SECRET_KEY } = require('../../middleware/config');
 
-//post method for registration
+const userController = {
+  //method for registration
+  createAccount: async function (ctx) {
+    const { email, username, password } = ctx.request.body;
+    const newUser = await User.findOne({ email: email });
+    if (newUser) {
+      ctx.body = 'User with that email already exists.';
+      ctx.status = 409;
+    }
+  }
+  //post? method for login
 
-//post? method for login
+  //logging out?
 
-//logging out?
+}
+
+module.exports = userController;
