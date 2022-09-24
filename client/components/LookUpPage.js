1f6019c9ae90a6e11c7a40ec52b00ec7c5cc6c57
M <v4nd3rp03l3ns1s@gmail.com>
2022-09-24 10:50:29 -0500

    feat: created dropdown component

--- client/components/LookUpPage.js
+++ client/components/LookUpPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Dropdown } from 'react-native-material-dropdown';
+import { trainService } from '../services/trainService';
 
 const LookUpPage = () => {
   return (
     <View style={styles.lookUpContainer}>
       <Text style={styles.lookUpCaptions}>Look up CTA</Text>
+      <Dropdown label="Select El Line" data={trainService.getTrainLines} />
     </View>
   );
 };
---
+++ client/services/trainService.js
@@ -0,0 +1,27 @@
+
+const config = require('../../server/config');
+const baseURL = config.serverURL;
+
+export const trainService = {
+  getTrainLines: async function () {
+    return fetch(`${baseURL}/train/getAllTrainLines`)
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((err) => err);
+  },
+  getTrainStations: async function (line) {
+    return fetch(`${baseURL}/train/getLineStations?ln=${line}`)
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((err) => err);
+  },
+  getTrainStops: async function (line) {
+    return fetch(`${baseURL}/train/getLineStops?ln=${line}`)
+      .then((res) => res.json())
+      .then((data) => data)
+      .catch((err) => err);
+  },
+  getTrainPredict: function () {
+    return null;
+  },
+};
